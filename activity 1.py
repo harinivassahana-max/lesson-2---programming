525c119b2fdd636b59035241de8f1c192826f557
harinivassahana-max <harinivassahana@gmail.com>
2025-10-17 16:04:08 +1100

    Add files via upload

---
+++ activity 1.py
@@ -0,0 +1,11 @@
+#Print a Sentence
+print("Welcome to the world of programming")
+
+#Print a Number
+print(4)
+
+#Next line after print
+print("hello" ,5)
+
+#End argument of Print Statement
+print ("welcome to ", end="*")
---
+++ activity 2.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x = 5
+y = "Sahana"
+print (x)
+print (y)
+
+codingal = "codingal"
+print (codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
